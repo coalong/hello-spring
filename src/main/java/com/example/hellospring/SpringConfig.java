0be9dbfe3cefddb579a4f6ff89a3fd082422c8ad
coalong <minay0904@gmail.com>
2022-06-03 00:58:36 +0900

    + 순수 JDBC 실습

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,24 +1,35 @@
-//package com.example.hellospring;
-//
-//import com.example.hellospring.repository.MemberRepository;
-//import com.example.hellospring.repository.MemoryMemberRepository;
-//import com.example.hellospring.service.MemberService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//
-////  자바 코드로 스프링 빈 등록
-//@Configuration
-//public class SpringConfig {
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
+package com.example.hellospring;
+
+import com.example.hellospring.repository.JdbcMemberRepository;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+//  자바 코드로 스프링 빈 등록
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-//    }
-//
-//}
+        return new JdbcMemberRepository(dataSource);
+    }
+
+}
